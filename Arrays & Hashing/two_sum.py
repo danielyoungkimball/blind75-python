a6eb521b7489e22ab09e9c364a77b8b3f1a3c24e
Daniel Kimball <danielyoungkimball@outlook.com>
2025-02-03 12:00:36 -0700

    Quick update

--- Arrays & Hashing/two_sum.py
+++ Arrays & Hashing/two_sum.py
@@ -1,3 +1,17 @@
 # Two Sum - LeetCode 1
 # https://leetcode.com/problems/two-sum/
 
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}  # Store numbers and their indices
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i  # Store the index of the current number
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.twoSum([2, 7, 11, 15], 9))  # Output: [0, 1]
