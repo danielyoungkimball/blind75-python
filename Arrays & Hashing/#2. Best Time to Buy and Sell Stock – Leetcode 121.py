4f5e6b7b2892d43a10d957d1ebf23352d960477e
Daniel Kimball <danielyoungkimball@outlook.com>
2025-02-05 14:43:01 -0700

    init #2

---
+++ Arrays & Hashing/#2. Best Time to Buy and Sell Stock – Leetcode 121.py
@@ -0,0 +1,8 @@
+# Best Time to Buy and Sell Stock - LeetCode 121
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        return
