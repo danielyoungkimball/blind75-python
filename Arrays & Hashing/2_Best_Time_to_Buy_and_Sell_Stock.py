be943dbb833b7a3a6d63c27a1ba8fd91eef43b80
Daniel Kimball <danielyoungkimball@outlook.com>
2025-02-14 10:31:45 -0700

    2

--- Arrays & Hashing/2_Best_Time_to_Buy_and_Sell_Stock.py
+++ Arrays & Hashing/2_Best_Time_to_Buy_and_Sell_Stock.py
@@ -3,6 +3,28 @@
 
 from typing import List
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        return
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        # -> arr[int]
+        # return int "profit"
+
+        profit = 0
+        min_value = prices[0]
+
+        for i , value in enumerate(prices):
+            # set min
+            if value < min_value:
+                min_value = value
+            
+            if (value - min_value > profit):
+                profit = (value - min_value)
+
+        return profit
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.maxProfit([7,1,5,3,6,4]))  # Output: 5
