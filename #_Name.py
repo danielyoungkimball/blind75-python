d5b9c299451d2a5c234b0a460c5151713d10f532
Daniel Kimball <danielyoungkimball@outlook.com>
2025-02-18 11:35:45 -0700

    Add template

---
+++ #_Name.py
@@ -0,0 +1,14 @@
+# INSERT_NAME - Leetcode INSERT_NUMBER
+# https://leetcode.com/problems/INSERT_NAME/
+
+# INSERT_NAME INSERT_PARAMS INSERT_RETURN INSERT_EXAMPLE
+
+from typing import List
+
+class Solution:
+    def INSERT_NAME(self, INSERT_PARAMS) -> INSERT_RETURN:
+        return
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.INSERT_NAME(INSERT_EXAMPLE))  # Output: 
