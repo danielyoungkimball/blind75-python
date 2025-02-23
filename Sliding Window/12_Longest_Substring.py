5af481cb64d5451e974a2d33d81674b278ca07e5
Daniel Kimball <danielyoungkimball@outlook.com>
2025-02-23 00:48:40 -0700

    Longest Substring

---
+++ Sliding Window/12_Longest_Substring.py
@@ -0,0 +1,22 @@
+# Longest Substring - 3
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        def check(start, end):
+            chars = set()
+            for i in range(start, end + 1):
+                c = s[i]
+                if c in chars:
+                    return False
+                chars.add(c)
+            return True
+
+        n = len(s)
+
+        res = 0
+        for i in range(n):
+            for j in range(i, n):
+                if check(i, j):
+                    res = max(res, j - i + 1)
+        return res
