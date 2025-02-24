9549379995230aa0302a49813de33de696f676d0
Daniel Kimball <danielyoungkimball@outlook.com>
2025-02-24 12:27:54 -0700

    Kth Largest Element

---
+++ Heap/18_Kth_Largest_Element.py
@@ -0,0 +1,20 @@
+# Kth Largest Element - Leetcode 215
+# https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+import heapq
+
+
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
+        min_heap = []
+        for num in nums:
+            heapq.heappush(min_heap, num)
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+        return min_heap[0]
