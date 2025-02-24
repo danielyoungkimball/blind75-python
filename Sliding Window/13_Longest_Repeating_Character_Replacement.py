9024359feb8443fed1b117bf14cc8ee903ed343c
Daniel Kimball <danielyoungkimball@outlook.com>
2025-02-24 11:50:44 -0700

    Longest Substring Repeating Character Replacement

--- Sliding Window/13_Longest_Repeating_Character_Replacement.py
+++ Sliding Window/13_Longest_Repeating_Character_Replacement.py
@@ -2,21 +2,22 @@
 # https://leetcode.com/problems/longest-repeating-character-replacement/
 
 class Solution(object): 
+   class Solution(object):
     def characterReplacement(self, s, k):
-        count = {}
-        max_count = 0
-        left = 0
-        maxLength = 0
-        
-        for right in range(len(s)):
-            count[s[right]] = count.get(s[right], 0) + 1
-            max_count = max(max_count, count[s[right]])
-            
-            # If window size minus count of most frequent character is greater than k, shrink window
-            while (right - left + 1) - max_count > k:
-                count[s[left]] -= 1
-                left += 1
-            
-            maxLength = max(maxLength, right - left + 1)
-        
-        return maxLength
+        counter = {}
+        res = 0
+
+        l = 0
+        for r in range(len(s)):
+            counter[s[r]] = 1 + counter.get(s[r], 0) # +1 to hash counter otherwise init at 0
+
+            while (r - l + 1) - max(counter.values()) > k:
+                # if length of substring minus the most common letter > k replacements
+                # then shrink left until valid 
+                counter[s[l]] -= 1
+                l += 1
+
+            # compare max res to current substring
+            # and reiterate
+            res = max(res, r - l + 1)
+        return res
